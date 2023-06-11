1370b6421781957cd6846f7d4d6dfa8080fca58d
Manson <manson972@yahoo.com>
2023-06-11 19:28:04 +0800

    custom theme update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import { Button, Typography, styled } from "@mui/material";
-import { Settings, Add } from "@mui/icons-material";
+
 
 function App() {
-  const BlueButton = styled(Button)({
-    backgroundColor: "skyblue",
-    color: "#888",
-    margin: 5,
-    "&:hover": {
-      backgroundColor: "lightblue",
-    },
-    "&:disabled": {
-      backgroundColor: "gray",
-      color: "white"
-    }
-  }) 
 
   return (
-    <div className="App">
-      <Button variant="text">Text</Button>
-      <Button startIcon={<Settings />} variant="contained" color="secondary" size="small">Contained</Button>
-      <Button startIcon={<Add />} variant="contained" color="success" size="small">Add new post</Button>
-      <Button variant="outlined" disabled>Outlined</Button>
-      <Typography variant="h1" component="p">
-        h1. Heading
-      </Typography>
-      <BlueButton>my button</BlueButton>
-      <BlueButton>my button</BlueButton>
+    <div>
+
     </div>
   );
 }
