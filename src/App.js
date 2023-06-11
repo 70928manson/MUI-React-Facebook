300a3400b3a57c75f4c450ac584e965e1c9af0e7
Manson <manson972@yahoo.com>
2023-06-11 13:56:04 +0800

    MUI sx prop test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { Button, Typography } from "@mui/material";
+import { Settings, Add } from "@mui/icons-material";
+
 function App() {
   return (
     <div className="App">
-      Hello
+      <Button variant="text">Text</Button>
+      <Button startIcon={<Settings />} variant="contained" color="secondary" size="small">Contained</Button>
+      <Button startIcon={<Add />} variant="contained" color="success" size="small">Add new post</Button>
+      <Button variant="outlined" disabled>Outlined</Button>
+      <Typography variant="h1" component="p">
+        h1. Heading
+      </Typography>
+      <Button variant="contained" disabled sx={{
+        backgroundColor: "skyblue",
+        color: "#888",
+        margin: 5,
+        "&:hover": {
+          backgroundColor: "lightblue",
+        },
+        "&:disabled": {
+          backgroundColor: "gray",
+          color: "white"
+        }
+      }}>My button</Button>
     </div>
   );
 }
