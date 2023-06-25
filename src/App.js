81b3cd032f35c6968afc071c9a109fd53db09afa
Manson <manson972@yahoo.com>
2023-06-25 21:45:01 +0800

    navbar with mui appbar update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import { Box, Stack } from "@mui/material";
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
 import Rightbar from "./components/Rightbar";
+import { Box, createTheme, Stack, ThemeProvider } from "@mui/material";
 import Navbar from "./components/Navbar";
-
 
 function App() {
 
+  const theme = createTheme({
+    palette: {
+
+    },
+  });
   return (
-    <Box>
-       <Navbar />
-       <Stack direction="row" spacing={2} justifyContent={"space-between"}>
-        <Sidebar />
-        <Feed />
-        <Rightbar />
-       </Stack>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar/>
+          <Feed />
+          <Rightbar />
+        </Stack>
+      </Box>
+    </ThemeProvider>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,59 @@
+import styled from '@emotion/styled';
+import { AppBar, Avatar, Badge, Box, InputBase, Toolbar, Typography } from '@mui/material';
+import { Mail, Notifications, Pets } from "@mui/icons-material";
 import React from 'react';
+
+const StyledToolbar = styled(Toolbar)({
+    display: "flex",
+    justifyContent: "space-between",
+});
+
+const Search = styled("div")(({theme}) => ({
+    backgroundColor: "white",
+    padding: "0 10px",
+    borderRadius: theme.shape.borderRadius
+}));
+
+const Icons = styled(Box)(({theme}) => ({
+   display: "none",
+   gap: "20px",
+   alignItems: "center",
+   [theme.breakpoints.up("sm")]: {
+    display: "flex"
+   }
+}));
+
+const UserBox = styled(Box)(({theme}) => ({
+    display: "flex",
+    alignItems: "center",
+    gap: "10px",
+    [theme.breakpoints.up("sm")]: {
+        display: "none"
+    }
+}));
 
 function Navbar(props) {
     return (
-        <div>
-            navbar
-        </div>
+        <AppBar position="sticky">
+            <StyledToolbar>
+                <Typography variant='h6' sx={{ display: { xs: "none", sm: "block" } }}>Manson Dev</Typography>
+                <Pets sx={{ display: { xs: "block", sm: "none" } }} />
+                <Search><InputBase placeholder='search...' /></Search>
+                <Icons>
+                    <Badge badgeContent={4} color="error">
+                        <Mail />
+                    </Badge>
+                    <Badge badgeContent={4} color="error">
+                        <Notifications />
+                    </Badge>
+                    <Avatar sx={{ width: "30px", height: "30px", }} src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+                </Icons>
+                <UserBox>
+                    <Avatar sx={{ width: "30px", height: "30px", }} src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+                    <Typography variant="span">Manson</Typography>
+                </UserBox>
+            </StyledToolbar>
+        </AppBar>
     );
 }
 
